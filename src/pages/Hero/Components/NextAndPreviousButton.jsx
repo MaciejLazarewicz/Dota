32e71f345365c2566c05ac395d16b0187db4b786
Maciej Lazarewicz <maciej.lazarewicz@gmail.com>
2023-12-06 17:09:58 +0100

    Feat: SingleHeroBreakpoints

--- src/pages/Hero/Components/NextAndPreviousButton.jsx
+++ src/pages/Hero/Components/NextAndPreviousButton.jsx
@@ -1,7 +1,13 @@
 import { Box, Image } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
+import { useBreakpoint } from '../../../components/constants/BreakPoints';
 
 function NextAndPreviousButton({ previousHero, nextHero }) {
+  const [isBreakPoint1200] = useBreakpoint('isBreakPoint1200');
+  const arrowBoxStyles1200 = {
+    width: '20px',
+    height: '45px',
+  };
   const arrowBoxStyles = {
     display: 'flex',
     alignItems: 'center',
@@ -16,6 +22,12 @@ function NextAndPreviousButton({ previousHero, nextHero }) {
         filter: 'brightness(1.0)',
       },
     },
+    ...(isBreakPoint1200 && arrowBoxStyles1200),
+  };
+
+  const arrowImageStyles1200 = {
+    width: '10px',
+    height: '22px',
   };
   const arrowImageStyles = {
     filter: 'brightness(0.5)',
@@ -23,12 +35,34 @@ function NextAndPreviousButton({ previousHero, nextHero }) {
     width: '15px',
     height: '32px',
     src: 'https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/arrow_solid_left.png',
+    ...(isBreakPoint1200 && arrowImageStyles1200),
+  };
+
+  const chessButtonBoxStyles1200 = {
+    width: '50px',
+    height: '25px',
+    gap: '4px',
+    alignContent: 'center',
+    justifyContent: 'center',
+    padding: '10px',
+  };
+
+  const chessButtonBoxStyles = {
+    width: '70px',
+    height: '45px',
+    gap: '8px',
+    padding: '9.5px',
+    border: '2px solid #888',
+    cursor: 'pointer',
+    display: 'grid',
+    gridTemplateColumns: 'repeat(3,1fr)',
+    ...(isBreakPoint1200 && chessButtonBoxStyles1200),
   };
 
   return (
     <Box
       position="absolute"
-      right="100"
+      right="20"
       top="200"
       display="flex"
       flexDir="row"
@@ -43,14 +77,7 @@ function NextAndPreviousButton({ previousHero, nextHero }) {
 
       <Link to="/Heroes">
         <Box
-          display="grid"
-          width="70px"
-          height="45px"
-          gap="8px"
-          padding="9.5"
-          gridTemplateColumns="repeat(3,1fr)"
-          border="2px solid #888"
-          cursor="pointer"
+          style={chessButtonBoxStyles}
           _hover={{
             borderColor: '#fff',
             '& .inner-box': {
@@ -61,8 +88,8 @@ function NextAndPreviousButton({ previousHero, nextHero }) {
           {[1, 2, 3, 4, 5, 6].map((index) => (
             <Box
               key={index}
-              width="16px"
-              height="16px"
+              width={isBreakPoint1200 ? '12px' : '16px'}
+              height={isBreakPoint1200 ? '12px' : '16px'}
               backgroundColor="#888"
               className="inner-box"
             />
